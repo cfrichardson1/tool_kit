59b3906615dc3cc8d2cfd705771f452c1d1630d1
Chris <cfrichardson1@gmail.com>
2019-04-19 11:14:12 -0700

    base commit

---
+++ basic.py
@@ -0,0 +1,11 @@
+import time
+
+def sleep_timer(a,b):
+    time.sleep(random.randint(a,b))
+    
+from difflib import SequenceMatcher
+
+def match_ratio(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+    

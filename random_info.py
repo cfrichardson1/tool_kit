4a9b6b628d6a87c697cdd1dfb7ac992e3a877e40
cfrichardson1 <cfrichardson1@gmail.com>
2019-05-27 14:21:38 -0700

    added relevant notes

---
+++ random_info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 14:18:40 2019
+
+@author: crich
+
+Iterating through
+
+for index, row PD.DF.iterrows():
+
+for key, value dict.items():
+"""
+
+

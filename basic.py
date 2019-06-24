ee77aa30a164627d1348d608f805530d1f74e04d
cfrichardson1 <cfrichardson1@gmail.com>
2019-06-24 16:40:39 -0700

    test

--- basic.py
+++ basic.py
@@ -2,10 +2,10 @@ import time
 
 def sleep_timer(a,b):
     time.sleep(random.randint(a,b))
-    
+
 from difflib import SequenceMatcher
 
 def match_ratio(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-    
+    ### TEST TEST
